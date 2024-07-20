0bc1be6a0480c4ed64c0043ff2b57224dca552b4
Bahman <imbahmanranjbar@gmail.com>
2024-07-20 23:37:31 +0330

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Run
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnbrow = new System.Windows.Forms.Button();
+            this.btncan = new System.Windows.Forms.Button();
+            this.btnok = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Run.Properties.Resources._4217b874458f68d88650001052824fa2;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("LIONELLO DEMO", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(375, 67);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Type the name of a program ,  folder , document or internet resource  , and windo" +
+    "ws will open it for you.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("LIONELLO DEMO", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(67, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Open :";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.btnbrow);
+            this.panel1.Controls.Add(this.btncan);
+            this.panel1.Controls.Add(this.btnok);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 126);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(475, 52);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnbrow
+            // 
+            this.btnbrow.Location = new System.Drawing.Point(344, 17);
+            this.btnbrow.Name = "btnbrow";
+            this.btnbrow.Size = new System.Drawing.Size(91, 23);
+            this.btnbrow.TabIndex = 0;
+            this.btnbrow.Text = "browse...";
+            this.btnbrow.UseVisualStyleBackColor = true;
+            this.btnbrow.Click += new System.EventHandler(this.btnbrow_Click);
+            // 
+            // btncan
+            // 
+            this.btncan.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btncan.Location = new System.Drawing.Point(245, 17);
+            this.btncan.Name = "btncan";
+            this.btncan.Size = new System.Drawing.Size(91, 23);
+            this.btncan.TabIndex = 0;
+            this.btncan.Text = "cancel";
+            this.btncan.UseVisualStyleBackColor = true;
+            this.btncan.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnok
+            // 
+            this.btnok.Location = new System.Drawing.Point(147, 17);
+            this.btnok.Name = "btnok";
+            this.btnok.Size = new System.Drawing.Size(91, 23);
+            this.btnok.TabIndex = 0;
+            this.btnok.Text = "ok";
+            this.btnok.UseVisualStyleBackColor = true;
+            this.btnok.Click += new System.EventHandler(this.btnok_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(147, 85);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(288, 28);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnok;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.CancelButton = this.btncan;
+            this.ClientSize = new System.Drawing.Size(475, 178);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("LIONELLO DEMO", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Run";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnbrow;
+        private System.Windows.Forms.Button btncan;
+        private System.Windows.Forms.Button btnok;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Run
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            Process.Start(comboBox1.Text);
+            comboBox1.Items.Remove(comboBox1.Text);
+            comboBox1.Items.Add(comboBox1.Text);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+               
+            }
+        
+        }
+
+        private void btnbrow_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.ShowDialog();
+            op.Title = "Browse  ";
+            comboBox1.Text = op.FileName;
+        }
+     
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            if (!File.Exists(Application.StartupPath+"applist.txt"))
+            {
+                File.CreateText(Application.StartupPath + "applist.txt");
+
+            }
+            else
+            {
+                string[] list = File.ReadAllLines(Application.StartupPath + "applist.txt");
+                for (int i = 0; i < list.Length; i++)
+                {
+                    comboBox1.Items.Add(list[i]);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            string[] list = new string[comboBox1.Items.Count];
+            for (int i = 0; i < comboBox1.Items.Count; i++)
+            {
+                list[i] = comboBox1.Items[i].ToString();
+            }
+                File.WriteAllLines(Application.StartupPath + "applist.txt", list);
+        }
+    }
+}
